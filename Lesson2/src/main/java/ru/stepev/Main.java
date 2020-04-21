6d2ba16be175cb08a853883ab61284a3f03f192d
Stepanov <websafari@yandex.ru>
2020-04-21 13:22:33 +0300

    second lesson

--- Lesson2/src/main/java/ru/stepev/Main.java
+++ Lesson2/src/main/java/ru/stepev/Main.java
@@ -8,6 +8,8 @@ public class Main {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         TestBean testBean = context.getBean("testBean", TestBean.class);
+        MyBean myBean = context.getBean("myBean",MyBean.class);
         System.out.println(testBean.getName());
+        System.out.println(myBean.getName() + " | " + myBean.getValue());
     }
 }
